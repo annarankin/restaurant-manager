73c4e87d863ff567c68220060b2ed65c0f9ebb4c
anna rankin <anna@annarankin.com>
2015-05-04 15:35:12 -0400

    prepping for deeeeeployment

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -254,7 +254,7 @@ function expandMenu(restaurantId, event) {
           var query = $(el).parents('.menu-item').find('[data-id ="name"]').text().replace(/ /g, "%20")
             // debugger
           $.ajax({
-            url: "https://api.nutritionix.com/v1_1/search/" + query + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=fae2a401&appKey=fe781f4f2b30d67668dc634ab4529e43",
+            url: "https://api.nutritionix.com/v1_1/search/" + query + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId="+appId+"&appKey="+ apikey,
             type: "GET",
             success: function(data) {
               console.log(data.hits[0].fields.item_name + ":\n" + data.hits[0].fields.nf_calories + " calories per " + data.hits[0].fields.nf_serving_size_unit)
---
+++ public/scripts/scratch.js
@@ -0,0 +1,7 @@
+$.ajax({
+  url: "https://api.nutritionix.com/v1_1/search/zucchini?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=fae2a401&appKey=fe781f4f2b30d67668dc634ab4529e43",
+  type: "GET",
+  success: function(data) {
+    console.log(data.hits[0].fields.item_name + ":\n" + data.hits[0].fields.nf_calories + " calories per " + data.hits[0].fields.nf_serving_size_unit)
+  }
+})
